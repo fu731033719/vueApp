9e0cb6c19349187388bd20304b0bb03f0819bd64
fu731033719 <15706804793@163.com>
2018-06-30 11:52:26 +0800

    项目目录构建

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    test word
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  #app
+    color : $color-theme
+</style>
